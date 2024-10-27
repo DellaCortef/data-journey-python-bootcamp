1899b9f301105f4de8a2e9f722053d68e55c1dbd
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-27 12:25:59 -0300

    exercise 01 and exercise 02

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ print(3 + 6)
 
 ## input() command
 print(input("Hello, please" + " enter your name: "))
+
+## exercise 01 - Create a program that allows the user to type their name and return the number of characters
+name = input("Enter your name: ")
+print(len(name))
+
+## exercise 02 - Input two values and add them. print the result
+value_1 = input("Enter a value: ")
+value_2 = input("Enter another value: ")
+sum = int(value_1) + int(value_2)
+print(sum)
