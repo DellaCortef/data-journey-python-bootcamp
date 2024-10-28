21f166ead1cb4a5859831335f195b2ee2351aa45
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-10-28 19:52:24 -0300

    FIX: fixing the operators of the int exercises

---
+++ module02/data_type_exercises.py
@@ -0,0 +1,45 @@
+# Integers (`int`)
+
+## 1. Write a program that adds two integers entered by the user.
+value1 = input("Please, enter a integer number: ")
+value2 = input("Please, enter another integer number: ")
+total1 = int(value1) + int(value2)
+print(total1)
+
+## 2. Create a program that receives a number from the user and calculates the remainder of dividing that number by 5.
+value3 = input("Please, enter a integer number: ")
+total2 = int(value3) % 5
+print(total2)
+
+## 3. Develop a program that multiplies two numbers provided by the user and displays the result.
+value4 = input("Please, enter a integer number: ")
+value5 = input("Please, enter another integer number: ")
+total3 = int(value4) * int(value5)
+print(total3)
+
+## 4. Write a program that asks for two integers and prints the integer division of the first by the second.
+value6 = input("Please, enter a integer number: ")
+value7 = input("Please, enter aanother integer number: ")
+total4 = int(value6) / int(value7)
+print(total4)
+
+## 5. Write a program that calculates the square of a user-supplied number.
+value8 = input("Please, enter a integer number: ")
+total5 = int(value8) ** 2
+print(total5)
+
+# Floating Point Numbers (`float`)
+
+## 6. Write a program that receives two floating numbers and adds them.
+
+
+## 7. Create a program that calculates the average of two user-supplied floating numbers.
+
+
+## 8. Develop a program that calculates the power of a number (base and exponent provided by the user).
+
+
+## 9. Write a program that converts the temperature from Celsius to Fahrenheit.
+
+
+## 10. Write a program that calculates the area of ​​a circle, taking the radius as input.
