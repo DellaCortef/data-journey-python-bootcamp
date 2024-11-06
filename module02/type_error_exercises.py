5441398f27fa9b41f19e73d3deb70f6e0f37180c
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-11-05 22:27:44 -0300

    creating simple calculator with if-else try-except

--- module02/type_error_exercises.py
+++ module02/type_error_exercises.py
@@ -13,22 +13,39 @@
 #     print("Inputed data is not a number. Please, enter a valid number!")
 
 ## 22: Palindrome Checker
-inputed_data = input("Please enter a word so we can test whether it's a palindrome: ")
-if isinstance(inputed_data, str):
-    print(f"The inputed data {inputed_data} is a word. Let's check!")
-    # Convert the word to lowercase to ensure case insensitivity
-    formated_data = inputed_data.replace(" ", "").lower()
-    # Check if the word is equal to its reverse
-    if formated_data == formated_data[::-1]:
-        print(f"The word {inputed_data} is a palindrome!")
-    else:
-        print(f"The word {inputed_data} is not a palindrome! Try again!")
-else:
-    print(f"The inputed data {inputed_data} is not a word. Try again!")
+# inputed_data = input("Please enter a word so we can test whether it's a palindrome: ")
+# if isinstance(inputed_data, str):
+#     print(f"The inputed data {inputed_data} is a word. Let's check!")
+#     # Convert the word to lowercase to ensure case insensitivity
+#     formated_data = inputed_data.replace(" ", "").lower()
+#     # Check if the word is equal to its reverse
+#     if formated_data == formated_data[::-1]:
+#         print(f"The word {inputed_data} is a palindrome!")
+#     else:
+#         print(f"The word {inputed_data} is not a palindrome! Try again!")
+# else:
+#     print(f"The inputed data {inputed_data} is not a word. Try again!")
 
 
 ## 23: Simple Calculator
+number1  = float(input("Please, enter a number: "))
+number2  = float(input("Please, enter another number: "))
+operator =       input("Which operator would you like to use? ")
 
+try:
+    if   operator == "+":
+        result = number1 + number2
+    elif operator == "-":
+        result = number1 - number2
+    elif operator == "*":
+        result = number1 * number2
+    elif operator == "/" and number2 != 0:
+        result = number1 / number2
+    else:
+        print("Invalid operator or division by 0.")
+    print(f"The result is {result}")
+except:
+    print("Error: Invalid entry. Make sure you enter numbers.")
 
 ## 24: Number Sorter
 
